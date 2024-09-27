52b409c91b5047c9f12c01a9c1bac79e0d9a8f60
inhadeepblue <deepblue@inha.ac.kr>
2024-09-27 15:15:12 +0900

    v0.2 IO, math, string

---
+++ week05/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt" // c언어의 #include <stdio.h> 역할
+	"math"
+	"strings"
+)
+
+func main() {
+	//var i int = 55
+	var f float32 = 3.99
+
+	// var i int
+	// i = 55
+
+	i := 55
+	fmt.Println(f, math.Ceil(3.49))
+	fmt.Println(strings.Title("kim inha"))
+	fmt.Printf("i는 %d\n", i)
+	fmt.Print("i는 ", i, "\n")
+	fmt.Println("i는", i)
+}
