61c49dabb7e993a2be50ac518cf4d7c89f90eb4d
inhadeepblue <deepblue@inha.ac.kr>
2024-09-27 15:22:54 +0900

    v0.3 Type

--- week05/main.go
+++ week05/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt" // c언어의 #include <stdio.h> 역할
 	"math"
+	"reflect"
 	"strings"
 )
 
 func main() {
 	//var i int = 55
-	var f float32 = 3.99
+	//var f float32 = 3.99
 
 	// var i int
 	// i = 55
 
 	i := 55
+	//i := "55"
+	f := 3.99
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 	fmt.Println(f, math.Ceil(3.49))
 	fmt.Println(strings.Title("kim inha"))
 	fmt.Printf("i는 %d\n", i)
