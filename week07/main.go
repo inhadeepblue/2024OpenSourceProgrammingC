8a4d418557eec74e05196fd4cfc4b1483aa4f129
inhadeepblue <deepblue@inha.ac.kr>
2024-10-11 15:27:36 +0900

    v1.3 Parse

--- week07/main.go
+++ week07/main.go
@@ -5,16 +5,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	fmt.Print("Input your score : ")
 	in := bufio.NewReader(os.Stdin)
-	fmt.Print("Input your name : ")
-	name, err := in.ReadString('\n')
+	i, err := in.ReadString('\n')
 
 	if err != nil {
 		log.Fatal(err)
+	}
+	i = strings.TrimSpace(i)                // 줄바꿈등 제거. 파이썬의 strip 함수와 비슷
+	score, _ := strconv.ParseInt(i, 16, 32) // 16진수 정수형(32bit)으로 변환
+	if score >= 60 {
+		fmt.Println("A")
+		fmt.Printf("%d\n", score)
 	} else {
-		fmt.Println(name)
+		fmt.Println("BCDF")
+		fmt.Printf("%d\n", score)
 	}
 }
