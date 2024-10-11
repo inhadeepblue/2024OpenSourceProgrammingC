77cdb72f3b5ca4772b21dcd33e334ab7f7a070cd
inhadeepblue <deepblue@inha.ac.kr>
2024-10-11 14:00:58 +0900

    v0.9 Time

--- week07/main.go
+++ week07/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
+	// 오늘은 2024년 10월 11일 입니다.
 	var now time.Time = time.Now()
-	var month int = int(now.Month()) //var month time.Month = now.Month()
-	fmt.Println(month)
+	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", now.Year(), int(now.Month()), now.Day())
+	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다.\n", now.Hour(), now.Minute(), now.Second())
 }
