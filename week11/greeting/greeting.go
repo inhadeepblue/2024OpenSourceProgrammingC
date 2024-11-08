a3f5ce22ff92d581456d59fde9c177e0ac9f27c4
inhadeepblue <deepblue@inha.ac.kr>
2024-11-08 14:35:41 +0900

    v3.4 Package

---
+++ week11/greeting/greeting.go
@@ -0,0 +1,11 @@
+package greeting
+
+import "fmt"
+
+func Hi(name string) {
+	fmt.Printf("Hi %s!\n", name)
+}
+
+func Hello(name string) {
+	fmt.Printf("Hello %s~\n", name)
+}
--- week11/main.go
+++ week11/main.go
@@ -1,64 +1,8 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-)
-
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	} else if n == 2 {
-		return true
-	} else if n%2 == 0 {
-		return false
-	} else {
-		j := 3
-		for j*j <= n {
-			if n%j == 0 {
-				return false
-			}
-			//fmt.Printf("%d ", j)
-			j = j + 2
-		}
-	}
-	return true
-}
-
-func getInteger() (int, error) {
-	in := bufio.NewReader(os.Stdin)
-	a, err := in.ReadString('\n')
-	if err != nil {
-		//log.Fatal(err)
-		return 0, err
-	}
-	a = strings.TrimSpace(a)
-	n, err := strconv.Atoi(a)
-	if err != nil {
-		return 0, err
-	}
-	return n, nil
-}
+import "week11/greeting"
 
 func main() {
-	fmt.Print("Input start number : ")
-	n1, err := getInteger()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Print("Input end number : ")
-	n2, err := getInteger()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for j := n1; j <= n2; j++ {
-		if isPrime(j) {
-			fmt.Printf("%d ", j)
-		}
-	}
+	greeting.Hello("Inha")
+	greeting.Hi("Harvard")
 }
