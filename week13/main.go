6652d3b3c3fe3e07d526f13f52ce8abba941a316
inhadeepblue <deepblue@inha.ac.kr>
2024-11-22 15:19:56 +0900

    v4.5 Slice

--- week13/main.go
+++ week13/main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"reflect"
 )
+
+// func test(strs string) {
+func test(strs ...string) {
+	fmt.Println(strs, reflect.TypeOf(strs))
+}
 
 func main() {
 	//fmt.Println(os.Args[1:], len(os.Args))
@@ -14,4 +20,8 @@ func main() {
 	}
 	slices = append(slices, "forever", "!")
 	fmt.Println(slices, len(slices))
+	test("abc")
+	test("abc", "123")
+	test()
+	test("abc", "123", "inha")
 }
