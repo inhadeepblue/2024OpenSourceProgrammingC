ced56d434091c84560697b51e591f3fb391f99dd
inhadeepblue <deepblue@inha.ac.kr>
2024-11-29 14:23:35 +0900

    v4.6 Map

---
+++ week14/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	ages := make(map[string]int)
+
+	var name string
+	var age int
+
+	for {
+		fmt.Print("What's ur name? (exit to 'q') ")
+		fmt.Scanln(&name)
+		if name == "q" || name == "Q" {
+			break
+		}
+		fmt.Print("Ur age? ")
+		fmt.Scanln(&age)
+
+		ages[name] = age
+	}
+	for name, age := range ages {
+		fmt.Printf("%s is %d year old.\n", name, age)
+	}
+}
