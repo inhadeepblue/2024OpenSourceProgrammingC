cadb17042785031881a1c5a3f5436ca0639e61ad
inhadeepblue <deepblue@inha.ac.kr>
2024-11-29 14:51:16 +0900

    v4.8 Struct

--- week14/main.go
+++ week14/main.go
@@ -2,21 +2,19 @@ package main
 
 import "fmt"
 
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student1 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student1 student
 	student1.id = 20241234
 	student1.name = "강철중"
 	student1.gpa = 4.5
 	fmt.Println(student1.gpa)
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student2 student
 	student2.id = 20244321
 	student2.name = "아이다"
 	student2.gpa = 4.43
