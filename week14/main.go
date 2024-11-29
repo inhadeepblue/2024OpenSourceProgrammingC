00ea9db254a894e3b424e2165a495d7b2d840392
inhadeepblue <deepblue@inha.ac.kr>
2024-11-29 15:21:23 +0900

    v4.9 Struct

--- week14/main.go
+++ week14/main.go
@@ -2,21 +2,39 @@ package main
 
 import "fmt"
 
-type student struct {
-	id   int
-	name string
-	gpa  float32
+type visitor struct {
+	age  int
+	cost int
+}
+
+func calculateCost(visitors []visitor) int {
+	// visitors : 구조체 슬라이스
+	totalCost := 0
+	for _, v := range visitors {
+		totalCost = totalCost + v.cost
+	}
+	return totalCost
 }
 
 func main() {
-	var student1 student
-	student1.id = 20241234
-	student1.name = "강철중"
-	student1.gpa = 4.5
-	fmt.Println(student1.gpa)
-	var student2 student
-	student2.id = 20244321
-	student2.name = "아이다"
-	student2.gpa = 4.43
-	fmt.Println(student2.id)
+	var numVisitors int
+	fmt.Println("How many visitors? ")
+	fmt.Scanln(&numVisitors)
+
+	vs := make([]visitor, numVisitors) // create Slice
+
+	for i := 0; i < numVisitors; i++ {
+		var age int
+		fmt.Print("Input age : ")
+		fmt.Scan(&age)
+
+		if age < 12 {
+			vs[i] = visitor{age: age, cost: 5000}
+		} else if age >= 12 && age < 65 {
+			vs[i] = visitor{age: age, cost: 10000}
+		} else {
+			vs[i] = visitor{age: age, cost: 7000}
+		}
+	}
+	fmt.Printf("Total price is %d won!", calculateCost(vs))
 }
